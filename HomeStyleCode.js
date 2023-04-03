c13437d9c81376d5fd5840187fc6db74c60a2b23
yannzin <hanzinyankee4@gmail.com>
2023-04-03 01:39:18 -0500

    added page load effect, split into separate files
    added a cool effect when the page loads where the screen is blocked with a picture in the middle. the logo gets hidden then the block leaves. also split the html into separate html css and js files.

---
+++ HomeStyleCode.js
@@ -0,0 +1,59 @@
+let pog;
+let pognum;
+let text;
+let ogText;
+let textSpot;
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+function textHypi() {
+  if (pog) {
+    return; // interval already set
+  }
+  pog = setInterval(textHypi2, 30);
+  pognum = 0;
+  text = document.getElementById("Title").innerHTML;
+  ogText = document.getElementById("Title").innerHTML;
+  textSpot = 0;
+  console.log("activated");
+}
+
+function textHypi2() {
+  console.log("running");
+  let textArray = text.split("");
+  for (let i = textSpot; i < text.length; i++) {
+    textArray[i] = alphabet[Math.floor(Math.random() * 52)];
+  }
+  text = textArray.join("");
+  pognum++;
+  if (pognum >= 5) {
+    textArray[textSpot] = ogText.charAt(textSpot);
+    text = textArray.join("");
+    textSpot++;
+    pognum = 0;
+  }
+  if (textSpot >= text.length) {
+    clearInterval(pog);
+    pog = null;
+  }
+  document.getElementById("Title").innerHTML = text;
+}
+
+window.onmousemove = e => {
+    const x = e.clientX - document.getElementById("cursorEffect").offsetWidth/2,
+          y = e.clientY - document.getElementById("cursorEffect").offsetHeight/2;
+    document.getElementById("cursorEffect").style.transform = `translate(${x}px, ${y}px)`;
+}
+
+window.onload = e => {
+  setTimeout(function() {
+    document.getElementById("openLogo").style.transition = "500ms cubic-bezier(.45,.21,.49,.8) transform";
+    document.getElementById("openLogo").style.transform = "translate(50vw,40vh)"
+  },
+  1000
+  )
+  setTimeout(function() {
+    document.getElementById("open").style.transform = "translateY(-101vh)";
+  }, 
+  1500
+  )
+}
